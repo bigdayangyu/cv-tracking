65bf367424b82411161e6a71dc2f64c6505cbc8d
Zoe <lizoezhe@gmail.com>
2019-12-03 00:00:05 -0500

    added template img

--- util/tracker_gui_test.py
+++ util/tracker_gui_test.py
@@ -17,12 +17,12 @@ import dataset_pyqtgraph
 import numpy as np
 from PyQt5.QtCore import Qt
 import time
+
 class MainUI(object):
     def __init__(self, MainWindow, data_path):
 
         MainWindow.setWindowTitle('Tracking demo')
         MainWindow.resize(1000, 700)
-
         # data path 
         self.gt_path = data_path
         self.path = data_path
@@ -31,21 +31,58 @@ class MainUI(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox.setGeometry(QtCore.QRect(10, 10, 120, 135))
+        self.groupBox.setGeometry(QtCore.QRect(10, 10, 150, 200))
         self.groupBox.setObjectName("groupBox")
 
-        self.panda_radBtn = QtWidgets.QRadioButton(self.groupBox)
-        self.panda_radBtn.setGeometry(QtCore.QRect(10, 20, 95, 20))
-        self.panda_radBtn.setObjectName("panda_radBtn")
-        self.panda_radBtn.toggled.connect(self.setDataset)
+        self.groupBox_legend = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_legend.setGeometry(QtCore.QRect(10, 220, 200, 300))
+        self.groupBox_legend.setObjectName("groupBox_legend")
+         
+        # self.legend_label = QtWidgets.QLabel()
+        self.legend_label = QtWidgets.QLabel(self.groupBox_legend)
+        self.legend_label.setGeometry(QtCore.QRect(10,20, 95, 20))
+        self.legend_label.setText("Ground Trut ----")
+        self.legend_label.setStyleSheet(" color: rgba(255, 0, 0); font-size: 10pt; font-weight: 300;")
+        
+        self.legend_label2 = QtWidgets.QLabel(self.groupBox_legend)
+        self.legend_label2.setText("Tracking Result ----")
+        self.legend_label2.setStyleSheet(" color: rgba(0,255, 0); font-size: 10pt; font-weight: 300;")
 
-        self.tiger_radBtn = QtWidgets.QRadioButton(self.groupBox)
-        self.tiger_radBtn.setGeometry(QtCore.QRect(10, 40, 95, 20))
-        self.tiger_radBtn.setObjectName("tiger_radBtn")
-        self.tiger_radBtn.toggled.connect(self.setDataset)
+        
+        # label.setText("Notes about sample ")
+        # layout = QtWidgets.QVBoxLayout()
+        # layout.addWidget(label)
+        # MainWindow.setLayout(layout)
+
+        self.radBtn_1 = QtWidgets.QRadioButton(self.groupBox)
+        self.radBtn_1.setGeometry(QtCore.QRect(10, 20, 95, 20))
+        self.radBtn_1.setObjectName("panda_radBtn")
+        self.radBtn_1.toggled.connect(self.setDataset)
+
+        self.radBtn_2 = QtWidgets.QRadioButton(self.groupBox)
+        self.radBtn_2.setGeometry(QtCore.QRect(10, 40, 95, 20))
+        self.radBtn_2.setObjectName("tiger_radBtn")
+        self.radBtn_2.toggled.connect(self.setDataset)
+
+
+        self.radBtn_3 = QtWidgets.QRadioButton(self.groupBox)
+        self.radBtn_3.setGeometry(QtCore.QRect(10, 60, 95, 20))
+        self.radBtn_3.setObjectName("panda_radBtn")
+        self.radBtn_3.toggled.connect(self.setDataset)
+
+        self.radBtn_4 = QtWidgets.QRadioButton(self.groupBox)
+        self.radBtn_4.setGeometry(QtCore.QRect(10, 80, 95, 20))
+        self.radBtn_4.setObjectName("tiger_radBtn")
+        self.radBtn_4.toggled.connect(self.setDataset)
+
+
+        self.radBtn_5 = QtWidgets.QRadioButton(self.groupBox)
+        self.radBtn_5.setGeometry(QtCore.QRect(10, 100, 95, 20))
+        self.radBtn_5.setObjectName("panda_radBtn")
+        self.radBtn_5.toggled.connect(self.setDataset)
 
         self.model_pulldown = QtGui.QComboBox(self.groupBox)
-        self.model_pulldown.setGeometry(QtCore.QRect(15, 65, 93, 28))
+        self.model_pulldown.setGeometry(QtCore.QRect(15,125, 93, 28))
         self.model_pulldown.setObjectName("model_pulldown")
         # self.model_pulldown.addItem("Select")
         self.model_pulldown.addItem("KCF")
@@ -55,7 +92,7 @@ class MainUI(object):
         self.model_pulldown.activated.connect(self.setModel)
 
         self.display_Btn = QtWidgets.QPushButton(self.groupBox)
-        self.display_Btn.setGeometry(QtCore.QRect(15, 100, 93, 28))
+        self.display_Btn.setGeometry(QtCore.QRect(15, 155, 93, 28))
         self.display_Btn.setObjectName("display_Btn")
 
         # Set up GraphicsLayoutWidget for images
@@ -97,9 +134,11 @@ class MainUI(object):
         # parameter for text display 
         param_dict = {'color':(255,255,255),
                       'anchor':(0,1)}
-        label_score = pg.TextItem(text='Score Map', **param_dict)
-        label_gt = pg.TextItem(text='Ground Truth', **param_dict)
+        label_score = pg.TextItem(text='Tracking Result', **param_dict)
+        label_gt = pg.TextItem(text='Tracking Error', **param_dict)
         label_ref = pg.TextItem(text='Reference Image', **param_dict)
+
+
         font.setPointSize(16)
         label_score.setFont(font)
         label_gt.setFont(font)
@@ -107,10 +146,14 @@ class MainUI(object):
         label_score.setParentItem(self.score_map)
         label_gt.setParentItem(self.groundtruth_img)
         label_ref.setParentItem(self.ref_img)
+
+
+        # label_score.setParentItem(self.score_map)
+
+
         self.score_box.addItem(label_score)
 
         self.ref_box.addItem(label_ref)
-
 
         # display buttons 
         self.display_Btn.clicked.connect(self.addImages)
@@ -135,36 +178,54 @@ class MainUI(object):
         sys.exit()
 
     def setDataset(self):
-        if self.panda_radBtn.isChecked():
+        if self.radBtn_1.isChecked():
             self.img_disp_path = "bolt1/"
+            self.template_path = "bolt.jpg"
+            self.temp_img =  self.load_temp()
             self.error_plot.removeItem(self.curve1) 
             self.error_data = self.read_error()
 
-        if self.tiger_radBtn.isChecked():
+        if self.radBtn_2.isChecked():
             self.img_disp_path  = "bolt2/"
+            self.template_path = "bolt2.jpg"
+            self.temp_img =  self.load_temp()
+
+            self.error_plot.removeItem(self.curve1) 
+            self.error_data = self.read_error()  
+
+        if self.radBtn_3.isChecked():
+            self.img_disp_path = "football/"
+            self.template_path = "football.jpg"
+            self.temp_img =  self.load_temp()
+            self.error_plot.removeItem(self.curve1) 
+            self.error_data = self.read_error()
+
+        if self.radBtn_4.isChecked():
+            self.img_disp_path  = "football1/"
+            self.template_path = "football1.jpg"
+            self.temp_img =  self.load_temp()
+            self.error_plot.removeItem(self.curve1) 
+            self.error_data = self.read_error()   
+
+        if self.radBtn_5.isChecked():
+            self.img_disp_path  = "mountainbike/"
+            self.template_path = "mountainbike.jpg"
+            self.temp_img =  self.load_temp()
             self.error_plot.removeItem(self.curve1) 
             self.error_data = self.read_error()   
 
     def setModel(self):
         if self.model_pulldown.currentText() == "KCF":
+            self.error_plot.removeItem(self.curve1) 
             self.img_root = self.path["KCF"]
         if self.model_pulldown.currentText() == "MDNet":
+            self.error_plot.removeItem(self.curve1) 
+
             self.img_root = self.path["MDNet"]
+
         if self.model_pulldown.currentText() == "SiamFC":
+            self.error_plot.removeItem(self.curve1) 
             self.img_root = self.path["SiamFC"]
-
-
-#             self.img_disp_path = self.path["bolt1"]
-#             self.error_plot.removeItem(self.curve1) 
-#             # self.curve1 = self.error_plot.plot(np.zeros((300, )))
-#             self.error_data = self.read_error()
-
-
-#         if self.tiger_radBtn.isChecked():
-#             self.img_disp_path  = self.path["bolt2"]
-#             self.error_plot.removeItem(self.curve1) 
-              
-#             self.error_data = self.read_error()   
 
     def addImages(self, MainWindow):
         # self.gt_path = self.path
@@ -186,10 +247,21 @@ class MainUI(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.groupBox.setTitle(_translate("MainWindow", "Select Dataset"))
-        self.panda_radBtn.setText(_translate("MainWindow", "Bolt"))
-        self.tiger_radBtn.setText(_translate("MainWindow", "Plastic"))
+        self.radBtn_1.setText(_translate("MainWindow", "Bolt1"))
+        self.radBtn_2.setText(_translate("MainWindow", "Bolt2"))
+                
+        self.radBtn_3.setText(_translate("MainWindow", "football"))
+
+        self.radBtn_4.setText(_translate("MainWindow", "football1"))
+        self.radBtn_5.setText(_translate("MainWindow", "mountainbike"))
         self.display_Btn.setText(_translate("MainWindow", "Display!"))
      
+    def load_temp(self):
+        imagePath = os.path.join(self.img_root + self.template_path )
+        temp = cv2.imread(imagePath)
+        temp = cv2.cvtColor(temp, cv2.COLOR_BGR2RGB)
+
+        return temp
     def load_data(self):
         n_files = len(os.listdir(self.img_root + self.img_disp_path)) - 3
         image_set = []
@@ -214,9 +286,9 @@ class MainUI(object):
 
     def updateData(self):
         self.score_map.setImage(self.data_set[self.i])
-        self.groundtruth_img.setImage(self.data_set[self.i])
+        # self.groundtruth_img.setImage(self.data_set[self.i])
 
-        self.ref_img.setImage(self.data_set[self.i])
+        self.ref_img.setImage(self.temp_img)
         self.i = (self.i + 1) % len(self.data_set)
        
         now = ptime.time()
@@ -234,15 +306,10 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
 
     win = QtWidgets.QMainWindow()
-    # path = {"bolt1" : './datasets/gui_dataset_mdnet/bolt1/', "bolt2":'./datasets/gui_dataset_kcf/bolt2/', 
-    #         "football":'./datasets/bolt2_kcf/bolt2/', "football1":'./datasets/bolt2_kcf/bolt2/', 
-    #         "mountainbike":'./datasets/bolt2_kcf/bolt2/'}
-
 
     path = {"KCF": './datasets/gui_dataset_kcf/', 
             "MDNet": './datasets/gui_dataset_mdnet/', 
             "SiamFC": './datasets/gui_dataset_siamfc/'}
-
 
     ui = MainUI(win, path)
 
